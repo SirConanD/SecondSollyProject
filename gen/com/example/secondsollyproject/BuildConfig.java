41e16eda9ba076cb829f449b6eaf65382b9a9b4b
SirConanD <fetzegra000@my.jordandistrict.org>
2014-10-09 09:50:39 -0600

    Add to repository.

---
+++ gen/com/example/secondsollyproject/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.secondsollyproject;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/secondsollyproject/WierdActivity.java
@@ -0,0 +1,55 @@
+package com.example.secondsollyproject;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WierdActivity extends Activity
+{
+	private Button appButton;
+	private TextView appText;
+	private ArrayList<Integer> colorList;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_wierd);
+		
+		appButton = (Button) findViewById(R.id.firstbutton);
+		appText = (TextView) findViewById(R.id.boringTextView);
+		colorList = new ArrayList<Integer>();
+		
+		fillTheColorList();
+		setUpListeners();
+	}
+	
+	private void fillTheColorList()
+	{
+		colorList.add(R.color.black);
+		colorList.add(R.color.prettyPurple);
+	}
+	
+	private void setUpListeners()
+	{
+		appButton.setOnClickListener(new View.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View currentView)
+			{
+				//Button click stuff goes here
+				int randomIndex = (int) (Math.random() * colorList.size());
+				
+				
+				appButton.setBackgroundResource(colorList.get(randomIndex));
+			}
+		});
+	}
+}
